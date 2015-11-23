6c4a56ab5222fbf9981539b8ddcddd21ce4c786b
clownvary <clownvary@gmail.com>
2015-11-23 14:58:39 +0800

    ok

--- TencentNewsHotLook/look.js
+++ TencentNewsHotLook/look.js
@@ -1,4 +1,5 @@
-var id=1250929750;
+var id=1250929750;//新闻id
+var num=50;//热评显示条数
 function getHotArr(data)
 {
 return  data.map(function(a,index){
@@ -6,6 +7,6 @@ return  data.map(function(a,index){
 return {"content":a.content,"up":a.up}; 
 });
 }
-$.get("http://coral.qq.com/article/"+id+"/hotcomment?reqnum=50",function(data){
-   getHotArr(JSON.parse(data).data.commentid).map(function(a,index){console.log((index+1)+"*"+a.content+"----"+a.up+"\n");});
+$.get("http://coral.qq.com/article/"+id+"/hotcomment?reqnum="+num,function(data){
+   getHotArr(JSON.parse(data).data.commentid).map(function(a,index){console.log((index+1)+"*"+a.content+"----赞"+a.up+"\n");});
 })
